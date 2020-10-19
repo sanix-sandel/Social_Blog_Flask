4563b02e9b9e36817a8702b99c09f580fe713702
sanix <sanicksikani@gmail.com>
2020-10-19 11:18:48 +0300

    Many to Many

--- app/models.py
+++ app/models.py
@@ -8,6 +8,12 @@ from app import login
 
 from hashlib import md5
 
+followers=db.Table('followers',
+
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 
 class User(UserMixin, db.Model):
     id=db.Column(db.Integer, primary_key=True)
@@ -17,6 +23,13 @@ class User(UserMixin, db.Model):
     posts=db.relationship('Post', backref='author', lazy='dynamic')
     about_me=db.Column(db.String(140))
     last_seen=db.Column(db.DateTime, default=datetime.utcnow)
+    
+    followed=db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id==id),
+        secondaryjoin=(followers.c.followed_id==id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
+    )
 
     def set_password(self, password):
         self.password_hash=generate_password_hash(password)
@@ -26,7 +39,20 @@ class User(UserMixin, db.Model):
 
     def avatar(self, size):
         digest=md5(self.email.lower().encode('utf-8')).hexdigest()
-        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"    
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"   
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id==user.id
+        ).count()>0                     
 
     def __repr__(self):
         return f'User {self.username}'
