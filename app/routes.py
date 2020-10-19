6b8dbc8a4ec66a2d58860636415bcd3263846516
sanix <sanicksikani@gmail.com>
2020-10-19 07:14:24 +0300

    Templates added

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world"
-    
+    user={'username':'Sanix'}
+    return render_template('index.html', 
+        title='Home', 
+        user=user)
