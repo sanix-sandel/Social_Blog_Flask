0424ecb2a4e18a9698f0ee009ee77d44055bd987
sanix <sanicksikani@gmail.com>
2020-10-19 10:49:52 +0300

    Edit profile

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,8 @@ from wtforms.validators import*
 from  email_validator import*
 from .models import User
 
+
+
 class LoginForm(FlaskForm):
     username=StringField('Username', validators=[DataRequired()])
     password=PasswordField('password1', validators=[DataRequired()])
@@ -27,3 +29,9 @@ class RegistrationForm(FlaskForm):
         user=User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.') 
+
+
+class EditProfileForm(FlaskForm):
+    username=StringField('Username', validators=[DataRequired()])
+    about_me=StringField('about_me', validators=[Length(min=0, max=140)])
+    submit=SubmitField('Submit')        
