da618b1e23d2a4b4edc62e121d80053dbd0502d8
sanix <sanicksikani@gmail.com>
2020-10-27 14:03:39 +0300

    Token based authentication

--- app/models.py
+++ app/models.py
@@ -83,7 +83,21 @@ class User(UserMixin, db.Model):
             'posts':[post.to_json_for_user() for post in self.posts]
             
         }    
-        return json_user         
+        return json_user   
+
+
+    def generate_auth_token(self, expiration):
+        s=Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id':self.id}).decode('utf-8') 
+
+    @staticmethod
+    def verify_auth_token(token):
+        s=Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data=s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])                     
 
     def __repr__(self):
         return f'User {self.username}'
